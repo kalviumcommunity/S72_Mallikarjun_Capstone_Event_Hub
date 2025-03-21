0a70a2a87f9a2ec1074dfc5d034c168c8cebca75
Mallikarjun722 <anandhiremath979@gmail.com>
2025-03-21 15:41:22 +0530

    add readme

--- index.js
+++ index.js
@@ -15,14 +15,20 @@ app.get('/api/welcome', (req, res) => {
 
 app.get('/categories/:category', (req, res) => res.json({ events: [] }));
 
+//{ "events": [ { "id": 1, "name": "Birthday Bash", "image": "birthday.jpg" } ] }
+
+
+
 
 app.get('/services/:eventId', (req, res) => {
     const { eventId } = req.params;
     res.json({ services: [] });
   });
   
+  //{ "services": ["Decoration", "Cake", "Food", "Transport"] }
+
 
 
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+    console.log(`Server is running on http://localhost:${PORT}`); 
 });
